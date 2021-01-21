822d3ff819e9f199c5c2c925d7934f4b36e200a0
fran <71708732+berniceluvscatz@users.noreply.github.com>
2021-01-21 21:37:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,75 @@
-from django.shortcuts import render
-from . models import *
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask("Filmzer")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+import json
+from daten import datenbank
+
+@app.route('/')
+def index():
+    return render_template('index.html', app_name="Filmzer")
+
+@app.route('/eingabe')
+def eingabe():
+    return render_template('eingabe.html', app_name="Eingabeformular")
+
+@app.route("/film/<name>", methods=['GET', 'POST'])
+def film(name):
+    filme_json = filmeladen()
+    titel=filme_json[name]["titel"]
+    film_lustig=filme_json[name]["film_lustig"]
+    film_traurig=filme_json[name]["film_traurig"]
+    film_spannend=filme_json[name]["film_traurig"]
+
+    if request.method == 'POST':
+        user_lustig = request.form['user_lustig']
+        user = request.form['produktname']
+        produktpreis = request.form['produktpreis']
+        antwort = speichern(zahl_1, produktname, produktpreis)
+        return render_template('resultat.html')
+        titel=titel,
+        deutscher_titel=deutscher_titel,
+        genre=genre,
+        laenge=laenge,
+        jahr=jahr,
+        regie=regie,
+        imdb_rating
+
+    return render_template('resultat.html')
+    titel=titel,\
+    deutscher_titel=deutscher_titel,\
+    genre=genre,\
+    laenge=laenge,\
+    jahr=jahr,\
+    regie=regie,
+    imdb_rating=imdb_rating
+    return render_template('index.html')
 
 # Create your views here.
 def index(request):
-    return render(request,'base/index.html')
+    return render(request,'index.html')
 
+def lustig(request):
+   category=['1']
+   film=filme.objects.filter(category=category)
+   return render(request,'eingabe.html')
 
-def Happy(request):
-    category=['1']
-    movie=Movies.objects.filter(category=category)
-    context = {'movie':movie}
-    return render(request,'base/happy.html',context)
-
-
-def Average(request):
+def traurig(request):
     category=['2']
-    movie=Movies.objects.filter(category=category)
-    context = {'movie':movie}
-    return render(request,'base/happy.html',context)
+    film=filme.objects.filter(category=category)
+    return render(request,'eingabe.html')
 
-def Sad(request):
+def spannend(request):
     category=['3']
-    movie=Movies.objects.filter(category=category)
-    context = {'movie':movie}
-    return render(request,'base/happy.html',context)
+    film=filme.objects.filter(category=category)
+    context = {'film':film}
+    return render(request,'eingabe.html',context)
 
-
-def Scale(request):
+def resultat(request):
     res = 0
     if request.method == 'POST':
         val1 = int(request.POST['num1'])
@@ -35,33 +78,5 @@ def Scale(request):
         res =  abs(10-(val1-val2))
 
     context = {'res':res}
-    return render(request,'base/scale.html',context)
+    return render(request,'eingabe.html',context)
 
-def Search(request):
-
-	query = request.GET['query']
-	products = Movies.objects.filter(funny_film__icontains=query)
-	# sad_film = Movies.objects.filter(sad_film__icontains=query)
-	# products = funny_film.union(sad_film)
-	context = {'products':products}
-	return render(request,'base/search.html', context)
-
-
-def Search1(request):
-
-	query = request.GET['query']
-
-	products = Movies.objects.filter(sad_film__icontains=query)
-	# products = funny_film.union(sad_film)
-	context = {'products':products}
-	return render(request,'base/search.html', context)
-
-
-def Search2(request):
-
-	query = request.GET['query']
-
-	products = Movies.objects.filter(film_intense__icontains=query)
-	# products = funny_film.union(sad_film)
-	context = {'products':products}
-	return render(request,'base/search.html', context)
